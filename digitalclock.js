c2a1f5f4c7fc2a5532b5f152ff0e4cfb385df77f
Majid Iqbal <8.majidiqbal@gmail.com>
2024-02-20 22:42:00 +0500

    clock2

--- digitalclock.js
+++ digitalclock.js
@@ -1,3 +1,20 @@
+// function startClock() {
+//     let clock = document.getElementById('clock');
+//     let time = new Date();
+//     let hours = time.getHours();
+//     let minutes = time.getMinutes();
+//     let seconds = time.getSeconds();
+//     let amPm = hours >= 12 ? 'PM' : 'AM';
+//     hours = hours % 12;
+//     hours = hours ? hours : 12;
+//     minutes = minutes < 10 ? '0' + minutes : minutes;
+//     seconds = seconds < 10 ? '0' + seconds : seconds;
+//     clock.textContent = hours + ':' + minutes + ':' + seconds + ' ' + amPm;
+//     setTimeout(startClock, 1000);
+// }
+
+// document.addEventListener('DOMContentLoaded', startClock);
+
 function startClock() {
     let clock = document.getElementById('clock');
     let time = new Date();
