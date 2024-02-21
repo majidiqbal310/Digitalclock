e12c7fcb525824869ef9c670864c3c4c19b15ee3
unknown <AJ@PC-11.AJ_TRUST>
2024-02-21 19:15:11 +0500

    updatedhsh

--- digitalclock.js
+++ digitalclock.js
@@ -1,3 +1,6 @@
+
+// document.addEventListener('DOMContentLoaded', startClock);
+
 // function startClock() {
 //     let clock = document.getElementById('clock');
 //     let time = new Date();
@@ -15,19 +18,37 @@
 
 // document.addEventListener('DOMContentLoaded', startClock);
 
-function startClock() {
-    let clock = document.getElementById('clock');
-    let time = new Date();
-    let hours = time.getHours();
-    let minutes = time.getMinutes();
-    let seconds = time.getSeconds();
-    let amPm = hours >= 12 ? 'PM' : 'AM';
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-    clock.textContent = hours + ':' + minutes + ':' + seconds + ' ' + amPm;
-    setTimeout(startClock, 1000);
-}
+// let fruitName = prompt("What fruit did You buy?")
 
-document.addEventListener('DOMContentLoaded', startClock);
+// switch (fruitName) {
+//     case "apple":
+//         console.log("Rs:300")
+//         break;
+
+//         case "mango":
+//         console.log("Rs:350")
+//         break;
+
+//         case "banana":
+//         console.log("Rs:200")
+//         break;
+
+//         case "lemon":
+//         console.log("Rs:250")
+//         break;
+
+//     default:
+//         alert("Not Found")
+//         break;
+// }
+
+// let i = 0 
+// while (i <= 25) {
+//     console.log(i)
+//     i++;
+// }
+let i = 0
+do { 
+console.log(i)
+i++
+} while (i <= 100);
